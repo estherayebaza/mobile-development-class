d30effc40bffc5c08d645cb0d34cb8b0fa622a5b
esther ayebaza <estherayebaza@gmail.com>
2021-02-21 10:59:28 +0300

    first commit

--- src/hello 2.kt
+++ src/hello 2.kt
@@ -1,4 +1,15 @@
 fun main() {
-    val name:String="esther"
-    print(name)
+    val studentName:String="esther ayebaza"
+    println(studentName)
+    var age :Int=18
+    println(age)
+    val phoneNumber="0776746882"
+    println(phoneNumber)
+    var weight=54.3556778
+    println(weight)
+    var citizenship= "false"
+    println(citizenship)
+    val name=arrayOf("esther ayebaza",18,"0776746882",54.3556778,"false")
+    println(name)
+
 }
