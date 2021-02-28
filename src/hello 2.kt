413e3c6899998fb224f4da6555c740f26c33e418
esther ayebaza <estherayebaza@gmail.com>
2021-02-28 12:00:33 +0300

    first commit

--- src/hello 2.kt
+++ src/hello 2.kt
@@ -1,15 +1,29 @@
 fun main() {
-    val studentName:String="esther ayebaza"
-    println(studentName)
-    var age :Int=18
-    println(age)
-    val phoneNumber="0776746882"
-    println(phoneNumber)
-    var weight=54.3556778
-    println(weight)
-    var citizenship= "false"
-    println(citizenship)
-    val name=arrayOf("esther ayebaza",18,"0776746882",54.3556778,"false")
-    println(name)
+  println("Hello Esther")
+   division()
+    add()
+    print("an ambitious girl")
 
 }
+fun division() {
+    var num1=37
+    var num2=5
+    var modulus=num1%num2
+    println(modulus)
+}
+fun add(){
+    var num1=10
+    var num2=20
+    var num3=5
+    var num4=23
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun reality() {
+    var reality=("an ambitious girl")
+
+
+}
+
+
+
